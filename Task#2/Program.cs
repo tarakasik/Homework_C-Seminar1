d77c8f5f41c81a2922ab24de23ed95392eb014dc
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-07 01:15:06 +0400

    add Task#2

---
+++ Task#2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+Console.WriteLine("Input first number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numA > numB)
+{ 
+Console.Write("Bigger = ");  
+Console.WriteLine(numA); 
+Console.Write("Lower = ");
+Console.WriteLine(numB);
+}
+else
+     {
+     Console.Write("Bigger = ");
+     Console.WriteLine(numB);
+     Console.Write("Lower = ");
+     Console.WriteLine(numA);  
+     }
